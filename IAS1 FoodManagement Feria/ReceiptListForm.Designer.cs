1ba365ec94032155f17a823d43b51da622b4d030
AprilleFeria@SMU <hed-ajferia@smu.edu.ph>
2025-12-03 22:51:47 +0800

    Added Database MySQL

---
+++ IAS1 FoodManagement Feria/ReceiptListForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace IAS1_FoodManagement_Feria
+{
+    partial class ReceiptListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvReceipts = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReceipts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvReceipts
+            // 
+            this.dgvReceipts.AllowUserToAddRows = false;
+            this.dgvReceipts.AllowUserToDeleteRows = false;
+            this.dgvReceipts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReceipts.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvReceipts.Location = new System.Drawing.Point(23, 12);
+            this.dgvReceipts.Name = "dgvReceipts";
+            this.dgvReceipts.RowHeadersVisible = false;
+            this.dgvReceipts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvReceipts.Size = new System.Drawing.Size(929, 391);
+            this.dgvReceipts.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(23, 409);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 63);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(148, 409);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(97, 63);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestore.Location = new System.Drawing.Point(23, 478);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(97, 63);
+            this.btnRestore.TabIndex = 3;
+            this.btnRestore.Text = "Restore";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(148, 478);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(97, 63);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(490, 444);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(171, 29);
+            this.txtSearch.TabIndex = 5;
+            this.txtSearch.Text = "search here";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(288, 444);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(97, 63);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // ReceiptListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 569);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dgvReceipts);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ReceiptListForm";
+            this.Text = "ReceiptListForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReceipts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvReceipts;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
---
+++ IAS1 FoodManagement Feria/backup_20251203_225057.sql
@@ -0,0 +1,118 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dbs_lab1
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `dbs_lab1`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `dbs_lab1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `dbs_lab1`;
+
+--
+-- Table structure for table `receipt_item_tbl`
+--
+
+DROP TABLE IF EXISTS `receipt_item_tbl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `receipt_item_tbl` (
+  `receipt_id` int DEFAULT NULL,
+  `food_item` varchar(100) DEFAULT NULL,
+  `quantity` decimal(10,0) DEFAULT NULL,
+  KEY `receipt_id` (`receipt_id`),
+  CONSTRAINT `receipt_item_tbl_ibfk_1` FOREIGN KEY (`receipt_id`) REFERENCES `receipts_tbl` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `receipt_item_tbl`
+--
+
+LOCK TABLES `receipt_item_tbl` WRITE;
+/*!40000 ALTER TABLE `receipt_item_tbl` DISABLE KEYS */;
+INSERT INTO `receipt_item_tbl` VALUES (16,'Barbecue',1),(16,'All Meaty',1),(18,'BaconCheese',1),(18,'Barbecue',2),(18,'StrawberryCheesecake',1),(18,'CookiesCream',1),(19,'BaconCheese',1),(19,'CookiesCream',2),(19,'Barbecue',1),(19,'All Meaty',1),(20,'BaconCheese',1),(20,'CookiesCream',2),(20,'Barbecue',1),(21,'ChocolateHeaven',1),(21,'BaconCheese',1),(21,'CookiesCream',1),(22,'ChocolateHeaven',1),(22,'BaconCheese',1),(22,'CookiesCream',2),(23,'All Meaty',1),(23,'StrawberryCheesecake',1),(24,'All Meaty',1),(24,'StrawberryCheesecake',1);
+/*!40000 ALTER TABLE `receipt_item_tbl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `receipts_tbl`
+--
+
+DROP TABLE IF EXISTS `receipts_tbl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `receipts_tbl` (
+  `payment` decimal(10,0) DEFAULT NULL,
+  `pay_change` decimal(10,0) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customerName` varchar(100) DEFAULT NULL,
+  `deliveryAddress` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `receipts_tbl`
+--
+
+LOCK TABLES `receipts_tbl` WRITE;
+/*!40000 ALTER TABLE `receipts_tbl` DISABLE KEYS */;
+INSERT INTO `receipts_tbl` VALUES (15000000,15000,1,'Richboy','Solano Mansion'),(15000000,15000,2,'Richboy','Solano Mansion'),(15000000,15000,3,'Richboy','Solano Mansion'),(120000,118500,7,'Please','Tae'),(120000,118820,8,'ff','dff'),(120000,118650,9,'s','s'),(120000,119400,10,'s','s'),(120000,119250,11,'s','s'),(120000,119230,12,'u','u'),(120000,119400,13,'ss','66'),(120000,119420,14,'y','42'),(120000,119020,15,'p','pp'),(120000,119150,16,'rr','1r'),(120000,118400,17,'TTe','fefe'),(120000,118460,18,'sfa','ssf'),(120000,118420,19,'Jejegirl','CR na mabantot'),(120000,118870,20,'nababaliw sa yo','langit ng mga tanga'),(120000,119250,21,'kulangot boy','sipon river'),(120000,119100,22,'dirty taong grasa','kabkab rain land ni Austine'),(700,90,23,'ostin','hsjhfjsd'),(700,90,24,'ostin','hsjhfjsd');
+/*!40000 ALTER TABLE `receipts_tbl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_user`
+--
+
+DROP TABLE IF EXISTS `tbl_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` longtext NOT NULL,
+  `role` enum('Admin','Staff','User') NOT NULL DEFAULT 'User',
+  `status` enum('Active','Pending','Inactive') NOT NULL DEFAULT 'Pending',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+LOCK TABLES `tbl_user` WRITE;
+/*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
+INSERT INTO `tbl_user` VALUES (5,'jose','jose1234','Admin','Pending'),(6,'pedro','pedro1234','User','Active'),(7,'prily','hahahahshshshshshshs','User','Inactive'),(9,'admin','3f28c9e5f49efe2ce08c056ec6ecc5d1','Admin','Active'),(10,'sssd','1153268148783bc42dba5e62ee638485','User','Pending'),(11,'dd','9306dd2bd7a22ee6691624490d78f866','User','Pending'),(12,'fffas','4f6be48299762969396d8def780070b1','User','Pending'),(22,'reggreer','9306dd2bd7a22ee6691624490d78f866','User','Pending');
+/*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-03 22:51:17
